e31a878f4003b1467f827a2b759b9af6115898e1
Agiangaspero <agiangaspero@celsius.com>
2025-04-30 14:23:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,6 @@ def synthesize():
     return jsonify({"audio_url": public_url})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
